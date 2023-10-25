caf03d600069cf093497fa7644380bff1c36cdfd
RudraNarola <rudrasnarola2@gmail.com>
2023-10-25 16:25:20 +0530

    tesing 2

---
+++ test.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+const int mod = 1e9 + 7;
+
+void solve()
+{
+}
+
+signed main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("d.txt", "w", stderr);
+#endif
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
